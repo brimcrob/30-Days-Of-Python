701d4172cf47ed8011f162be2fc976a91f41bdf6
Brian McRoberts <brimcrob@gmail.com>
2021-06-25 14:41:46 +0100

    MyDays

---
+++ Myday4/dy4Practice.py
@@ -0,0 +1,55 @@
+# # multiline string
+# multiline_string = '''I am a techer and enjoy teaching
+# I didnt find anything more rewarding as empowering people
+# that is why I created 30 days of python'''
+
+# print(multiline_string)
+
+# concatenation
+
+# first_name = 'Bob'
+# last_name = 'Smith'
+# space = ' '
+# full_name = first_name + space + last_name
+# print(full_name)
+
+# using the built in functions to check length of strings
+# print(len(first_name))
+# print(len(last_name))
+# print(len(first_name) > len(last_name))
+# print(len(last_name))
+
+# escape sequence
+# print('I hope you are enjoying yourself.\nAre you ?')
+# print('A\tB\tC\tD')
+# print('day 1\t3\t5')
+# print('day 2\t3\t5')
+# print('day 3\t3\t5')
+# print('day 4\t3\t5')
+# print('this is a baclash symbol (\\)')
+# print('every programming journey starts with \"hello world!\"')
+
+# formatting the old way
+# first_name = 'Brian'
+# last_name ='McRoberts'
+# language ='Python'
+# formatted_string ='I am %s %s, I am learning %s ' %(first_name, last_name, language)
+# print(formatted_string)
+#
+# radius = 10
+# pi = 3.14
+# area = pi * radius ** 2
+# new_string = 'the area of a circle with a radius of %d is %.2f' %(radius, area)
+# print(new_string)
+
+# formatting in version3 of python
+# first_name = 'Steven'
+# last_name = 'Jones'
+# language = 'Python'
+# formated_string = 'I am {} {}. I am learning {}'.format(first_name, last_name, language)
+# print(formated_string)
+# first_num = 4
+# second_num = 3
+#
+# print('{} + {} = {}'.format(first_num, second_num, first_num + second_num))
+# print('{} / {} = {:.2f}'.format(first_num, second_num, first_num / second_num))
